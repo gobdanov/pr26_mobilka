ada5cae6cafa52ace6135ebe848eea790da05e1f
gobdanov <gobdanov@inbox.ru>
2026-06-15 16:30:21 +0500

    программирование MainActivity. богданов

--- app/src/main/java/com/example/pr26_mobilka/MainActivity.java
+++ app/src/main/java/com/example/pr26_mobilka/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.pr26_mobilka;
 
+import android.content.Context;
+import android.hardware.biometrics.BiometricManager;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import classes.BiometricHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    BiometricHelper biometricHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        context = this;
+        Button btnLogin = findViewById(R.id.btnLogin);
+        biometricHelper = new BiometricHelper(this, callback);
+
+        btnLogin.setOnClickListener(v->{
+            biometricHelper.show();
         });
+    }
+
+    BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+        @Override
+        public void onAuthenticationFailed() {
+            super.onAuthenticationFailed();
+        }
+
+        @Override
+        public void onAuthenticationError(int errorCode, CharSequence errString) {
+            super.onAuthenticationError(errorCode, errString);
+        }
+
+        @Override
+        public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+            super.onAuthenticationSucceeded(result);
+            Toast.makeText(context, "авторизация пройдена", Toast.LENGTH_SHORT);
+        }
     }
 }
